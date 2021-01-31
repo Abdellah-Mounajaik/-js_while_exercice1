0bfae91fb17c9da8070ac6946eefc4859486c2d6
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:32:41 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+// exo1
+
+// let nbr = prompt(7)
+// let compt = 0
+// while (compt <= nbr) {
+//     console.log(compt);
+//     compt++
+// }
+
+// exo2
+
+// let nb = 0
+// while (nb <=8) {
+//     console.log(nb);
+//     nb++
+// }
+
+// let nbr = 3
+// while (nbr <=6) {
+//     console.log(nbr);
+//     nbr++
+//     if (nbr == 7) {
+//        break 
+//     }
+// }
+
+
+
+// Créer une boucle while qui affiche un console log avec le numéro du tour 
+// Créer une boucle while qui affiche un console log avec le numéro du tour en sachant que le tour doit commencer à 3 et s'arreter à 6
+// Créer une boucle qui affiche le numéro et qui s'incrémente de 2
